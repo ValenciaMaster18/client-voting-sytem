5a7aec31dc0db73e4bedf8f0d8e3195c37ffddd3
Luis valencia <luchito123410@gmail.com>
2023-04-08 17:37:55 -0500

    We created the pie chart in the statistics views and the csv file download functionality with voting and candidate data

--- src/app/module/admin/components/votaciones/view-voting/estadisticas/estadisticas.component.ts
+++ src/app/module/admin/components/votaciones/view-voting/estadisticas/estadisticas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IEstadisticas } from 'src/app/models/iestadisticas';
 import { VotacionService } from 'src/app/services/Votacion/votacion.service';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
 @Component({
   selector: 'app-estadisticas',
   templateUrl: './estadisticas.component.html',
@@ -10,7 +11,7 @@ export class EstadisticasVotingComponent implements OnInit {
   data: any;
   options: any;
   estadisticas!: IEstadisticas;
-
+  jsonCsv: any[] = [];
   constructor(
     private _votacionService: VotacionService
   ) {
@@ -25,10 +26,10 @@ export class EstadisticasVotingComponent implements OnInit {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
     this.data = {
-      labels: ['A', 'B', 'C'],
+      labels: Object.keys(this.estadisticas.votosPorCandidato),
       datasets: [
         {
-          data: [300, 50, 100],
+          data: Object.values(this.estadisticas.votosPorCandidato),
         }
       ]
     };
@@ -43,5 +44,42 @@ export class EstadisticasVotingComponent implements OnInit {
         }
       }
     };
+  }
+  exportCsv(): void {
+    this.estadisticas.candidatos!.forEach((candidato) => {
+      this.jsonCsv.push(
+        {
+          id: this.estadisticas.votacion?.id,
+          nombre: this.estadisticas.votacion?.nombre,
+          fecha: this.estadisticas.creationDateTime,
+          cantidadVotantesHabilitados: this.estadisticas.votantesHabilitados,
+          cantidadVotos: this.estadisticas.cantidadVotos,
+          documentoCandidato: candidato.documento,
+          votosCandidato: this.estadisticas.votosPorCandidato?.[candidato.documento]
+        }
+      )
+    })
+    let opciones = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: false,
+      showTitle: false,
+      title: 'Votaciones detalles',
+      useBom: false,
+      noDownload: false,
+      headers: [
+        "ID VOTACION",
+        "NOMBRE VOTACION",
+        "FECHA VOTACION",
+        "VOTANTES HABILITADOS",
+        "CANTIDAD VOTOS",
+        "NUMERO DOCUMENTO CANDIDATO",
+        "CANTIDAD VOTOS CANDIDATO"
+      ]
+    };
+
+    new ngxCsv(this.jsonCsv, "Report", opciones)
+    this.jsonCsv.splice(0, this.jsonCsv.length)
   }
 }
